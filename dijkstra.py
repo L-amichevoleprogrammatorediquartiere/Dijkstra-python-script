c85c9bbe38e2ea4356719187ce7ed570d935ccea
L-amichevoleprogrammatorediquartiere <64810680+L-amichevoleprogrammatorediquartiere@users.noreply.github.com>
2024-02-25 01:14:25 +0100

    Add files via upload

---
+++ dijkstra.py
@@ -0,0 +1,88 @@
+collegamenti_peso= None
+nodo_peso_success= []
+
+def inserimentoDati():
+    global collegamenti_peso
+    
+    collegamenti_peso=[['a','b', 5],
+                       ['a','c', 10],
+                       ['c','d', 2],
+                       ['d','b', 11],
+                       ['c','b', 3]]
+
+def calcolaPercorso(a):
+    global collegamenti_peso
+    global nodo_peso_success
+    nodi = []
+    for i in collegamenti_peso:
+        if(i[0] not in nodi):
+            nodi.append(i[0])
+
+        if(i[1] not in nodi):
+            nodi.append(i[1])
+            
+    #nodo - costo - nexthop
+    for i in nodi:
+        nodo_peso_success.append([i,-1,-1])
+        
+    #primo passo porre costo per i nodi adiacenti
+    #e porre i successori uguali al nodo che abbiamo scelto
+    assegnaCostoSucc(a,0,a)
+    nodi_vicini=adiacenzaNodi(a)
+    for i in nodi_vicini:
+        assegnaCostoSucc(i,calcolaPeso(a,i),a)
+
+    nodi_visti=[a]
+    for i in range(len(nodi)-1):
+        for nodi in nodi_vicini:
+            if(nodi not in nodi_visti):
+                nodo_scelto=nodi
+                break
+        nodi_vicini=adiacenzaNodi(nodo_scelto)
+        #nodi_vicini.remove(a)
+        for nodi in nodi_vicini:
+            assegnaCostoSucc(nodi,calcolaPeso(nodi,nodo_scelto)+
+                             calcolaCostoFinal(nodo_scelto),nodo_scelto)
+        nodi_visti.append(nodo_scelto)
+        
+def assegnaCostoSucc(n,c,s):
+    global nodo_peso_success
+    for i in nodo_peso_success:
+        if (i[0] == n):
+            if(i[1]>c or i[1] == -1):
+                i[1]=c
+                i[2]=s
+            break
+
+def calcolaCostoFinal(a):
+    global nodo_peso_success
+    for i in nodo_peso_success:
+        if(i[0]==a):
+            return i[1]
+
+def calcolaPeso(a,b):
+    global collegamenti_peso
+    for i in collegamenti_peso:
+        if((i[0] == a and i[1] == b) or (i[0] == b and i[1] == a)):
+            return i[2]
+
+def adiacenzaNodi(a):
+    nodi_vicini= []
+    global collegamenti_peso
+    for i in collegamenti_peso:
+        if(i[0]== a):
+            nodi_vicini.append(i[1])
+        elif(i[1]==a):
+            nodi_vicini.append(i[0])
+    return nodi_vicini
+
+def stampaRisultati():
+    global nodo_peso_success
+    print("nodo\tcosto\tsuccessivo")
+    for nodo in nodo_peso_success:
+        print(nodo[0],"\t",nodo[1],"\t",nodo[2])
+
+if __name__ == '__main__':
+    inserimentoDati()
+    calcolaPercorso("d")
+    stampaRisultati()
